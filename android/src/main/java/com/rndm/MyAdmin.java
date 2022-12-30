97891106ec81d47091d38cecb79ba6358c425f15
brefabu <40739200+brefabu@users.noreply.github.com>
2022-12-30 15:58:07 +0200

    Add files via upload

---
+++ android/src/main/java/com/rndm/MyAdmin.java
@@ -0,0 +1,8 @@
+package com.rndm;
+
+import android.app.admin.DeviceAdminReceiver;
+
+
+public class MyAdmin extends DeviceAdminReceiver{
+
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNDeviceManager } = NativeModules;
+
+export default RNDeviceManager;
