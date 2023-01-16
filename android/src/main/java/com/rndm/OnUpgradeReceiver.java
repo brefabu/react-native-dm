d221e8e7b13454741019183186f1a473d5e13472
Bunescu Fabian <bunescufabian@gmail.com>
2023-01-16 17:44:07 +0200

    update restart test v1.1.5

--- android/src/main/java/com/rndm/OnUpgradeReceiver.java
+++ android/src/main/java/com/rndm/OnUpgradeReceiver.java
@@ -1,5 +1,8 @@
 package com.rndm;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,9 +10,20 @@ import android.content.Intent;
 public class OnUpgradeReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
+        Activity mActivity = getCurrentActivity();
         Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage("com.project");
-        if (launchIntent != null) {
-            context.startActivity(launchIntent);
+
+      if (mActivity != null) {
+        DevicePolicyManager myDevicePolicyManager = (DevicePolicyManager) mActivity.getSystemService(Context.DEVICE_POLICY_SERVICE);
+        ComponentName mDPM = new ComponentName(mActivity, MyAdmin.class);
+
+        if (myDevicePolicyManager.isDeviceOwnerApp(mActivity.getPackageName())) {
+            myDevicePolicyManager.reboot(mDPM);
+
+            if (launchIntent != null) {
+                context.startActivity(launchIntent);
+            }
         }
+      }
     }
 }
